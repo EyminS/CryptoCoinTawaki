a0d5604cd301c1ccaa5f0b6be64f81064bea154a
Sylvie Eymin <EyminS@mail.com>
2017-02-12 03:41:20 +1300

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -4,3 +4,7 @@ const unsigned EMISSION_SPEED_FACTOR = 18;
 const uint64_t DIFFICULTY_TARGET = 120;
 const int P2P_DEFAULT_PORT = 64531;
 const int RPC_DEFAULT_PORT = 65531;
+const std::initializer_list<const char*> SEED_NODES = {
+  "111.11.11.11:17236",
+  "222.22.22.22:17236",
+};
