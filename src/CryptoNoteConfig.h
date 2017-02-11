a200042fa6c4a72943a5b504ed3cc1e451b404de
Sylvie Eymin <EyminS@mail.com>
2017-02-12 03:39:15 +1300

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -2,3 +2,5 @@ const char CRYPTONOTE_NAME[] = "Tawaki Coin";
 const uint64_t MONEY_SUPPLY = (uint64_t)(-1);
 const unsigned EMISSION_SPEED_FACTOR = 18;
 const uint64_t DIFFICULTY_TARGET = 120;
+const int P2P_DEFAULT_PORT = 64531;
+const int RPC_DEFAULT_PORT = 65531;
