2bdfce39f1512016a576769d4017d0608f6c81b1
Sylvie Eymin <EyminS@mail.com>
2017-02-12 03:31:17 +1300

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -1,3 +1,4 @@
 const char CRYPTONOTE_NAME[] = "Tawaki Coin";
 const uint64_t MONEY_SUPPLY = (uint64_t)(-1);
-const unsigned EMISSION_SPEED_FACTOR = 50;
+const unsigned EMISSION_SPEED_FACTOR = 18;
+const uint64_t DIFFICULTY_TARGET = 120;
